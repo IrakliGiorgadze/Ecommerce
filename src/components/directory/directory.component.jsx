0f1db6fc8f4fc5d0ea019871ce13c8ad5015219b
IrakliGiorgadze <ika.giorgadze@gmail.com>
2020-04-02 20:02:16 +0400

    styling menu-item

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -10,32 +10,36 @@ class Directory extends React.Component {
       sections: [
         {
           title: "hats",
-          imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+          imageUrl:
+            "https://i.pinimg.com/736x/1a/f0/49/1af049c5a09863dfca60b628495eb56a.jpg",
           id: 1,
           linkUrl: "shop/hats"
         },
         {
           title: "jackets",
-          imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+          imageUrl:
+            "https://img2.pngio.com/jean-jacket-club-penguin-rewritten-wiki-fandom-jacket-animated-png-1859_1375.png",
           id: 2,
           linkUrl: "shop/jackets"
         },
         {
           title: "sneakers",
-          imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+          imageUrl: "http://clipart-library.com/img1/774239.png",
           id: 3,
           linkUrl: "shop/sneakers"
         },
         {
           title: "womens",
-          imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+          imageUrl:
+            "https://i.pinimg.com/564x/15/7c/aa/157caab6a7704f2e221344f4613063a5.jpg",
           size: "large",
           id: 4,
           linkUrl: "shop/womens"
         },
         {
           title: "mens",
-          imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+          imageUrl:
+            "https://hips.hearstapps.com/ell.h-cdn.co/assets/17/11/480x363/odnxzi9puni2vrmjly8b.jpg?resize=480:*",
           size: "large",
           id: 5,
           linkUrl: "shop/mens"
@@ -46,8 +50,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} />
+        {this.state.sections.map(({ title, imageUrl, id, size }) => (
+          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
         ))}
       </div>
     );
