ac31b1192df31697e9093b17435d774d2f5caaba
IrakliGiorgadze <ika.giorgadze@gmail.com>
2020-04-23 23:10:17 +0400

    react-saga v1

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -21,7 +21,7 @@ export const fetchCollectionsFailure = (errorMessage) => ({
 
 export const fetchCollectionsStartAsync = () => {
   return (dispatch) => {
-    const collectionRef = firestore.collection("collections");
+    /* const collectionRef = firestore.collection("collections");
     dispatch(fetchCollectionsStart());
 
     collectionRef
@@ -30,6 +30,6 @@ export const fetchCollectionsStartAsync = () => {
         const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
         dispatch(fetchCollectionsSuccess(collectionsMap));
       })
-      .catch((error) => dispatch(fetchCollectionsFailure(error.message)));
+      .catch((error) => dispatch(fetchCollectionsFailure(error.message)));*/
   };
 };
